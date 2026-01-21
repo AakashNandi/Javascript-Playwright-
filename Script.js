bbf4c8d7e451825b4e2278b5553138e57e8af3b5
AakashNandi <aakashnandi3@gmail.com>
2026-01-21 09:10:17 +0000

    First Push

---
+++ Script.js
@@ -0,0 +1,61 @@
+console.log("Hello")
+let y=5;    //number
+y=10;  //retinitialize   it's now string
+//let y = 10;    //not possible can't Re-declare
+console.log(y)
+
+
+var a=5;
+var a="ABC";   //Re-Declare
+a=10;         //Re-Intiliaze
+console.log(a);
+
+
+const s = "CBA";
+//s="KCM"  //Can't Re-Initialize
+//const s= "KKK"  //Can't Re-Decclare
+console.log(s);
+
+const details = {                            //Object
+    name: "ALX",
+    designation: "Work",
+    year: 2026,
+    location:"Kolkata"
+};
+
+console.log(details);
+console.log(typeof(details));                  //output: object
+console.log(typeof(details.year));             //Number
+console.log(Array.isArray(details));           //False
+
+
+const arr = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(arr[0]);
+console.log(arr[4]);
+console.log(typeof(arr));               //object 
+console.log(Array.isArray(arr));       //true
+console.log(arr.indexOf('i'));
+arr.push('k');   //add at the end of array
+console.log(arr);
+arr.unshift('first');   //adds as the beginning
+console.log(arr);
+console.log(arr.length);
+arr.shift();   //removes the first element
+arr.pop();  //removes last element
+arr[0] = 'A';    //adding the data. existing will be overwritten
+var slice = arr.slice(1,3);
+console.log(slice);
+slice = arr.slice(2);
+console.log(slice);
+slice = arr.slice();
+console.log(slice);
+
+const obj = {a:"aakash", b:3, c:9}
+console.log("Hello" +obj);
+
+const data = ["A", "B", "E", "F", "G"];
+data.splice(2,0,"C","D");    //want to add C and D from second position...., why adding 0 there ?
+                            //Because 0 means no elements will be deleted after that position and 
+                            //if 1 then 1 will be deleted and 2 and so on...
+console.log(data);
